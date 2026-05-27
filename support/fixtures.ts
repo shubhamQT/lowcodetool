f2a24a80f6a7737a012fcde8eb6c5a83277cb729
Jagadeesh <jagadeesh@qtsolv.com>
2026-05-27 23:54:23 +0530

    sync: latest framework files

--- support/fixtures.ts
+++ support/fixtures.ts
@@ -1,11 +1,13 @@
 import { test as base, expect } from "@playwright/test";
 import { CartPage } from "../pageobjects/CartPage";
+import { CommonPage } from "../pageobjects/CommonPage";
 import { HomePage } from "../pageobjects/HomePage";
 import { ProductdetailPage } from "../pageobjects/ProductdetailPage";
 import { SearchdetailsPage } from "../pageobjects/SearchdetailsPage";
 
 type AppFixtures = {
   cartPage: CartPage;
+  commonPage: CommonPage;
   homePage: HomePage;
   productdetailPage: ProductdetailPage;
   searchdetailsPage: SearchdetailsPage;
@@ -14,6 +16,9 @@ type AppFixtures = {
 export const test = base.extend<AppFixtures>({
   cartPage: async ({ page }, use) => {
     await use(new CartPage(page));
+  },
+  commonPage: async ({ page }, use) => {
+    await use(new CommonPage(page));
   },
   homePage: async ({ page }, use) => {
     await use(new HomePage(page));
